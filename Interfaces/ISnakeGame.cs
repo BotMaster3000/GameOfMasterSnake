a4c3b6f17de0435382ea48f6af2621dc12398507
BotMaster3000 <jdgebauer@gmx.de>
2019-04-06 21:55:15 +0200

    Fixed ISnakeGame - IsPlayerGameOver now correctly returns bool

--- Interfaces/ISnakeGame.cs
+++ Interfaces/ISnakeGame.cs
@@ -24,7 +24,7 @@ namespace GameOfMasterSnake.Interfaces
         int TotalMoves { get; }
 
         void NextRound();
-        void IsPlayerGameOver();
+        bool IsPlayerGameOver();
         void PlaceSnakeOnMap();
         void SetSnakeDirection(Direction direction);
     }
