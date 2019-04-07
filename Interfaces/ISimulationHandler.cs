d2d54f7bae7edf8cf5ba0741e4d00c11b631de3c
BotMaster3000 <jdgebauer@gmx.de>
2019-04-07 20:33:29 +0200

    Added ISimulationHandler-Interface and partially implementated SimulationHandler

---
+++ Interfaces/ISimulationHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeuralBotMasterFramework.Interfaces;
+
+namespace GameOfMasterSnake.Interfaces
+{
+    public interface ISimulationHandler
+    {
+        IGeneticAlgorithm Algorithm { get; }
+        Type SimulationType { get; }
+
+        void SetAlgorithm(IGeneticAlgorithm algorithm);
+        void SetSimulationType(Type type);
+        void InitializeSimulation();
+    }
+}
---
+++ Simulations/SimulationHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameOfMasterSnake.Interfaces;
+using NeuralBotMasterFramework.Interfaces;
+
+namespace GameOfMasterSnake.Simulations
+{
+    public class SimulationHandler : ISimulationHandler
+    {
+        public IGeneticAlgorithm Algorithm { get; private set; }
+        public Type SimulationType { get; private set; }
+
+        private Dictionary<IGeneticAlgorithm, object> AlgorithmAndSimulationType { get; } = new Dictionary<IGeneticAlgorithm, object>();
+
+        public SimulationHandler() { }
+
+        public SimulationHandler(IGeneticAlgorithm algorithm, Type simulationType)
+        {
+            Algorithm = algorithm;
+            SimulationType = simulationType;
+        }
+
+        public void SetAlgorithm(IGeneticAlgorithm algorithm)
+        {
+            Algorithm = algorithm;
+        }
+
+        public void SetSimulationType(Type type)
+        {
+            SimulationType = type;
+        }
+
+        public void InitializeSimulation()
+        {
+            AlgorithmAndSimulationType.Clear();
+            for (int i = 0; i < Algorithm.TotalNetworks; ++i)
+            {
+                IWeightedNetwork network = Algorithm.NetworksAndFitness.Keys.ElementAt(i);
+                //AlgorithmAndSimulationType.Add(network, SimulationType.());
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
